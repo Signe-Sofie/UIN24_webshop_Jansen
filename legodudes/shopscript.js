66f41a1f24e3f985c020cd40e16c170a5c680535
Signe-Sofie <142736190+Signe-Sofie@users.noreply.github.com>
2025-01-16 09:55:57 +0100

    Update shopscript.js

--- legodudes/shopscript.js
+++ legodudes/shopscript.js
@@ -24,7 +24,7 @@ document.getElementById("shoppingcart").addEventListener("click", function() {
 
 //add product to cart
 function addProductToCart(prodid) {
-    console.log("du vil legge til produkt id " + prodid)
+    console.log("du vil legge til produktid " + prodid)
     //bruk .some for å sjekke om prodid allerede finnes i cart
     const idExists = cart.some(cartprod => cartprod.cartprodid === prodid)
 
@@ -38,6 +38,43 @@ function addProductToCart(prodid) {
         cart.push({cartprodid: prodid, quantity: 1})
     }
 
-
+    printCart()
     console.log(cart)
 }
+
+//lage en funksjon so, skriver ut oppdatert versjon av handlevognen
+function printCart() {
+    //starte med en tom variabel vi kan fylle med html
+    let cartHTML = ""
+    //lag klar variabel for pris:
+    let cartTotal = 0;
+    //lag variabel for anntal produkter
+    let cartNumber = 0;
+
+    //gå gjennom cart arrayen og generere HTML for hvert produkt
+    cart.map((cartprod, index) => {
+        const currentProduct = products.findIndex(p => p.prodid === cartprod.cartprodid)
+        const currentProductInfo = products[currentProduct]
+        cartHTML += `<article class="cart-product">
+                    <span class="title">${currentProductInfo.title}</span>
+                    <span class="price">${currentProductInfo.price},-</span>
+                    <span class="quantity">x<span class="quantity-number">${cartprod.quantity}</span></span>
+                    <button class="delete">x</button>
+                </article>`
+            //regn ut totalsum
+            cartTotal += currentProductInfo.price * cartprod.quantity
+            //regn ut antall produkter
+            cartNumber += cartprod.quantity
+    })
+
+    //skrive ut generert html til index fila:
+    document.getElementById("cart-products").innerHTML = cartHTML
+
+    //skrive ut totalpris
+    document.getElementById("cart-total").innerHTML = cartTotal
+
+    //skrive ut antall produkter
+    document.getElementById("cartcount").innerHTML = cartNumber
+}
+
+printCart()
