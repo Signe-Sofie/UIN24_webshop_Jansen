efc927144c26c033a0c2f54529cde08e5e0c3de7
Signe-Sofie <142736190+Signe-Sofie@users.noreply.github.com>
2025-01-16 09:28:03 +0100

    h

--- legodudes/shopscript.js
+++ legodudes/shopscript.js
@@ -11,8 +11,33 @@ products.map((product, index) => productHTML +=
                 <a href="#ketegoriside">Ninjago</a>
                 <h3>${product.title}</h3>
                 <p>Kr. ${product.price},-</p>
-                <button>Legg i handlekurv</button>
+                <button onclick="addProductToCart(${product.prodid})">Legg i handlekurv</button>
             </article>`)
 
 //finn #productlist, og fyll den med vardiene i variablen productHTML
 document.getElementById("productlist").innerHTML = productHTML
+
+//lage toggle funksjonalitet for handlevogn
+document.getElementById("shoppingcart").addEventListener("click", function() {
+    document.getElementById("cart").classList.toggle("visible")
+})
+
+//add product to cart
+function addProductToCart(prodid) {
+    console.log("du vil legge til produkt id " + prodid)
+    //bruk .some for å sjekke om prodid allerede finnes i cart
+    const idExists = cart.some(cartprod => cartprod.cartprodid === prodid)
+
+    if(idExists) {
+        //oppdatere dette produktets quantity
+        //først: finne indexen til denne id-en:
+        const index = cart.findIndex(p => p.cartprodid === prodid)
+        //så: oppdatere riktig quantity
+        cart[index].quantity++
+    } else {
+        cart.push({cartprodid: prodid, quantity: 1})
+    }
+
+
+    console.log(cart)
+}
